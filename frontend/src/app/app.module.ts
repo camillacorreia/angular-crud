7a7849c24233b73a83d4cff51c0761ddf7fc4a58
Camilla Correia <camillajesuscorreia@hotmail.com>
2021-04-08 19:29:46 -0300

    feat: generating table component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -27,6 +27,10 @@ import { ProductCreateComponent } from './components/product/product-create/prod
 
 import { HttpClientModule } from '@angular/common/http';
 import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
@@ -40,6 +44,7 @@ import { ProductReadComponent } from './components/product/product-read/product-
     ForDirective,
     ProductCreateComponent,
     ProductReadComponent,
+    ProductRead2Component,
   ],
   imports: [
     BrowserModule,
@@ -54,7 +59,10 @@ import { ProductReadComponent } from './components/product/product-read/product-
     HttpClientModule,
     FormsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
