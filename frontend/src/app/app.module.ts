36c198ac481e02034dc40b53cb49c46c59ce96f4
Camilla Correia <camillajesuscorreia@hotmail.com>
2021-04-08 15:54:33 -0300

    feat: structure directive

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { NavComponent } from './components/template/nav/nav.component';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { RedDirective } from './directives/red.directive';
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
-    RedDirective
+    RedDirective,
+    ForDirective,
   ],
   imports: [
     BrowserModule,
