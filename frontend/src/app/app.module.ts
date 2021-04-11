c4d3efa22421d3bada3088340a0f05724c76a67e
Camilla Correia <camillajesuscorreia@hotmail.com>
2021-04-11 12:10:10 -0300

    feat: delete product

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -35,6 +35,7 @@ import { MatSortModule } from '@angular/material/sort';
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
 import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 
 registerLocaleData(localePt);
 
@@ -52,6 +53,7 @@ registerLocaleData(localePt);
     ProductReadComponent,
     ProductRead2Component,
     ProductUpdateComponent,
+    ProductDeleteComponent,
   ],
   imports: [
     BrowserModule,
